5570dd5bf6ed0e42c568ef03d56cc59b49f403c1
Mohamed Badr <62759252+MohamedhBadr@users.noreply.github.com>
2022-03-28 14:20:37 +0200

    added some years and cleaned up the code a little

--- france.py
+++ france.py
@@ -9,6 +9,7 @@ and how they have changed over time.
 France was chosen given that it has a significant agricultural sector compared to the rest
 of Europe
 
+Note: The loading and parsining of the EXIOBASE input output tables can take some time. 
 """
 
 # %%
@@ -31,12 +32,36 @@ Stressor = 'CO2 - combustion - air'
 #Load EXIOBASE 1995
 exio3_1995 = pymrio.parse_exiobase3(path='exiobase3.4_iot_1995_pxp.zip')
 exio3_1995.calc_all()
+#Load EXIOBASE 1996
+exio3_1996 = pymrio.parse_exiobase3(path='IOT_1996_pxp.zip')
+exio3_1996.calc_all()
+#Load EXIOBASE 1997
+exio3_1997 = pymrio.parse_exiobase3(path='IOT_1997_pxp.zip')
+exio3_1997.calc_all()
+#Load EXIOBASE 1998
+exio3_1998 = pymrio.parse_exiobase3(path='IOT_1998_pxp.zip')
+exio3_1998.calc_all()
 #load EXIOBASE 1999
 exio3_1999 = pymrio.parse_exiobase3(path='IOT_1999_pxp.zip')
 exio3_1999.calc_all()
+#Load EXIOBASE 2000
+exio3_2000 = pymrio.parse_exiobase3(path='IOT_2000_pxp.zip')
+exio3_2000.calc_all()
+#Load EXIOBASE 2001
+exio3_2001 = pymrio.parse_exiobase3(path='IOT_2001_pxp.zip')
+exio3_2001.calc_all()
+#Load EXIOBASE 2002
+exio3_2002 = pymrio.parse_exiobase3(path='IOT_2002_pxp.zip')
+exio3_2002.calc_all()
 #Load EXIOBASE 2003
 exio3_2003 = pymrio.parse_exiobase3(path='IOT_2003_pxp.zip')
 exio3_2003.calc_all()
+#Load EXIOBASE 2004
+exio3_2004 = pymrio.parse_exiobase3(path='IOT_2004_pxp.zip')
+exio3_2004.calc_all()
+#Load EXIOBASE 2005
+exio3_2005 = pymrio.parse_exiobase3(path='IOT_2005_pxp.zip')
+exio3_2005.calc_all()
 #Load EXIOBASE 2008
 exio3_2008 = pymrio.parse_exiobase3(path='IOT_2008_pxp.zip')
 exio3_2008.calc_all()
@@ -53,12 +78,23 @@ exio3_2020.calc_all()
 exio3_2022 = pymrio.parse_exiobase3(path='IOT_2022_pxp.zip')
 exio3_2022.calc_all()
 
+# %%
+
+# %%
 
 # %%
 #Sattelite Accounts
 FR_Multipliers_1995 = exio3_1995.satellite.M[Country]
-FR_Multipliers_1999 = exio3_1995.satellite.M[Country]
+FR_Multipliers_1996 = exio3_1996.satellite.M[Country]
+FR_Multipliers_1997 = exio3_1997.satellite.M[Country]
+FR_Multipliers_1998 = exio3_1998.satellite.M[Country]
+FR_Multipliers_1999 = exio3_1999.satellite.M[Country]
+FR_Multipliers_2000 = exio3_2000.satellite.M[Country]
+FR_Multipliers_2001 = exio3_2001.satellite.M[Country]
+FR_Multipliers_2002 = exio3_2002.satellite.M[Country]
 FR_Multipliers_2003 = exio3_2003.satellite.M[Country]
+FR_Multipliers_2004 = exio3_2004.satellite.M[Country]
+FR_Multipliers_2005 = exio3_2005.satellite.M[Country]
 FR_Multipliers_2008 = exio3_2008.satellite.M[Country]
 FR_Multipliers_2011 = exio3_2011.satellite.M[Country]
 FR_Multipliers_2016 = exio3_2016.satellite.M[Country]
@@ -74,8 +110,16 @@ FR_Multipliers_2022 = exio3_2022.satellite.M[Country]
 #in the first 17 columns (should double check)
 
 FR_Agr_M_1995 = FR_Multipliers_1995.iloc[:, 0:17]
+FR_Agr_M_1996 = FR_Multipliers_1996.iloc[:, 0:17]
+FR_Agr_M_1997 = FR_Multipliers_1997.iloc[:, 0:17]
+FR_Agr_M_1998 = FR_Multipliers_1998.iloc[:, 0:17]
 FR_Agr_M_1999 = FR_Multipliers_1999.iloc[:, 0:17]
+FR_Agr_M_2000 = FR_Multipliers_2000.iloc[:, 0:17]
+FR_Agr_M_2001 = FR_Multipliers_2001.iloc[:, 0:17]
+FR_Agr_M_2002 = FR_Multipliers_2002.iloc[:, 0:17]
 FR_Agr_M_2003 = FR_Multipliers_2003.iloc[:, 0:17]
+FR_Agr_M_2004 = FR_Multipliers_2004.iloc[:, 0:17]
+FR_Agr_M_2005 = FR_Multipliers_2005.iloc[:, 0:17]
 FR_Agr_M_2008 = FR_Multipliers_2008.iloc[:, 0:17]
 FR_Agr_M_2011 = FR_Multipliers_2011.iloc[:, 0:17]
 FR_Agr_M_2016 = FR_Multipliers_2016.iloc[:, 0:17]
@@ -88,19 +132,27 @@ FR_Agr_M_2022 = FR_Multipliers_2022.iloc[:, 0:17]
 #Comparing carbon intensities of of the French agricultural sector from 1995-2011
 
 df1 = FR_Agr_M_1995.loc[Stressor]
-df2 = FR_Agr_M_1999.loc[Stressor]
-df3 = FR_Agr_M_2003.loc[Stressor]
-df4 = FR_Agr_M_2008.loc[Stressor]
-df5 = FR_Agr_M_2011.loc[Stressor]
-df6 = FR_Agr_M_2016.loc[Stressor]
-df7 = FR_Agr_M_2020.loc[Stressor]
-df8 = FR_Agr_M_2022.loc[Stressor]
+df2 = FR_Agr_M_1996.loc[Stressor]
+df3 = FR_Agr_M_1997.loc[Stressor]
+df4 = FR_Agr_M_1998.loc[Stressor]
+df5 = FR_Agr_M_1999.loc[Stressor]
+df6 = FR_Agr_M_2000.loc[Stressor]
+df7 = FR_Agr_M_2001.loc[Stressor]
+df8 = FR_Agr_M_2002.loc[Stressor]
+df9 = FR_Agr_M_2003.loc[Stressor]
+df10 = FR_Agr_M_2004.loc[Stressor]
+df11 = FR_Agr_M_2005.loc[Stressor]
+df12 = FR_Agr_M_2008.loc[Stressor]
+df13 = FR_Agr_M_2011.loc[Stressor]
+df14 = FR_Agr_M_2016.loc[Stressor]
+df15 = FR_Agr_M_2020.loc[Stressor]
+df16 = FR_Agr_M_2022.loc[Stressor]
 
-dfs = [df1, df2, df3, df4, df5, df6, df7, df8]
+dfs = [df1, df2, df3, df4, df5, df6, df7, df8, df9, df10, df11, df12, df13, df14, df15, df16]
 
 Merged_df = pd.concat(dfs, join='outer', axis=1).fillna(0)
 Merged_df = Merged_df.reset_index()
-Merged_df.columns.values[[1, 2, 3, 4, 5, 6, 7, 8]] = ['1995','1999','2003', '2008','2011','2016','2020','2022']
+Merged_df.columns.values[[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]] = ['1995','1996', '1997','1998', '1999','2000', '2001', '2002','2003','2004', '2005','2008','2011','2016','2020','2022']
 Merged_df = Merged_df.set_index('sector')
 
 
