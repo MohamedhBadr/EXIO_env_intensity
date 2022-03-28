07a335bacd68f354f9b641e155f49a854f16461a
Mohamed Badr <62759252+MohamedhBadr@users.noreply.github.com>
2022-03-28 11:11:59 +0200

    added some more years

--- france.py
+++ france.py
@@ -31,6 +31,9 @@ Stressor = 'CO2 - combustion - air'
 #Load EXIOBASE 1995
 exio3_1995 = pymrio.parse_exiobase3(path='exiobase3.4_iot_1995_pxp.zip')
 exio3_1995.calc_all()
+#load EXIOBASE 1999
+exio3_1999 = pymrio.parse_exiobase3(path='IOT_1999_pxp.zip')
+exio3_1999.calc_all()
 #Load EXIOBASE 2003
 exio3_2003 = pymrio.parse_exiobase3(path='IOT_2003_pxp.zip')
 exio3_2003.calc_all()
@@ -46,22 +49,23 @@ exio3_2016.calc_all()
 #load EXIOBASE 2020
 exio3_2020 = pymrio.parse_exiobase3(path='IOT_2020_pxp.zip')
 exio3_2020.calc_all()
+#load EXIOBASE 2022
+exio3_2022 = pymrio.parse_exiobase3(path='IOT_2022_pxp.zip')
+exio3_2022.calc_all()
 
 
 # %%
 #Sattelite Accounts
 FR_Multipliers_1995 = exio3_1995.satellite.M[Country]
+FR_Multipliers_1999 = exio3_1995.satellite.M[Country]
 FR_Multipliers_2003 = exio3_2003.satellite.M[Country]
 FR_Multipliers_2008 = exio3_2008.satellite.M[Country]
 FR_Multipliers_2011 = exio3_2011.satellite.M[Country]
 FR_Multipliers_2016 = exio3_2016.satellite.M[Country]
 FR_Multipliers_2020 = exio3_2020.satellite.M[Country]
+FR_Multipliers_2022 = exio3_2022.satellite.M[Country]
 
 
-# %%
-#Visualize M Matrix changes regarding Co2 Air combustion
-#Air = FR_Multipliers_1995.loc[Stressor]
-#Air.plot(figsize=(20,20), kind='barh')
 
 # %%
 
@@ -70,11 +74,13 @@ FR_Multipliers_2020 = exio3_2020.satellite.M[Country]
 #in the first 17 columns (should double check)
 
 FR_Agr_M_1995 = FR_Multipliers_1995.iloc[:, 0:17]
+FR_Agr_M_1999 = FR_Multipliers_1999.iloc[:, 0:17]
 FR_Agr_M_2003 = FR_Multipliers_2003.iloc[:, 0:17]
 FR_Agr_M_2008 = FR_Multipliers_2008.iloc[:, 0:17]
 FR_Agr_M_2011 = FR_Multipliers_2011.iloc[:, 0:17]
 FR_Agr_M_2016 = FR_Multipliers_2016.iloc[:, 0:17]
 FR_Agr_M_2020 = FR_Multipliers_2020.iloc[:, 0:17]
+FR_Agr_M_2022 = FR_Multipliers_2022.iloc[:, 0:17]
 
 
 # %%
@@ -82,17 +88,19 @@ FR_Agr_M_2020 = FR_Multipliers_2020.iloc[:, 0:17]
 #Comparing carbon intensities of of the French agricultural sector from 1995-2011
 
 df1 = FR_Agr_M_1995.loc[Stressor]
-df2= FR_Agr_M_2003.loc[Stressor]
-df3= FR_Agr_M_2008.loc[Stressor]
-df4 = FR_Agr_M_2011.loc[Stressor]
-df5 = FR_Agr_M_2016.loc[Stressor]
-df6 = FR_Agr_M_2020.loc[Stressor]
+df2 = FR_Agr_M_1999.loc[Stressor]
+df3 = FR_Agr_M_2003.loc[Stressor]
+df4 = FR_Agr_M_2008.loc[Stressor]
+df5 = FR_Agr_M_2011.loc[Stressor]
+df6 = FR_Agr_M_2016.loc[Stressor]
+df7 = FR_Agr_M_2020.loc[Stressor]
+df8 = FR_Agr_M_2022.loc[Stressor]
 
-dfs = [df1, df2, df3, df4, df5, df6]
+dfs = [df1, df2, df3, df4, df5, df6, df7, df8]
 
-Merged_df = pd.concat(dfs, join='outer', axis=1).fillna(nan_value)
+Merged_df = pd.concat(dfs, join='outer', axis=1).fillna(0)
 Merged_df = Merged_df.reset_index()
-Merged_df.columns.values[[1, 2, 3, 4, 5, 6]] = ['1995','2003', '2008','2011','2016','2020']
+Merged_df.columns.values[[1, 2, 3, 4, 5, 6, 7, 8]] = ['1995','1999','2003', '2008','2011','2016','2020','2022']
 Merged_df = Merged_df.set_index('sector')
 
 
@@ -125,18 +133,26 @@ which hence decrease emissions per euro/dollar.
 
 #Follow up question: Despite a general trend of decarbonisation, is this offset by and increase in final demand? 
 """
+# %%
+#Define French Final Demand
+Fr_Y = exio3_1995.Y.loc[Country]
+Fr_agr_Y = Fr_Y[0:17] #agricultural French final demand
+
 
 # %%
-"""
-From the above analyisis we can see that there is a general trend towards decorbonising the
-agricultural sector in France (with the exception of wool). 
-Keeping in mind that the indicator chosen here was 'C02- combustion- air'
+#Lets get Carbon footprint for 1995 from french Agriculture
+#Using D= M@Y (Needs another Look)
+D = FR_Agr_M_1995@Fr_agr_Y
+D
 
-I use the years 1995, 2008, 2011, and 2020, I think it could be interesting to witness the effects 
-of global economic shocks (2008 financial crisis, and 2020 covid) on carbon intensities.
-
-"""
 # %%
-#Get a time series visualization
+
+#uncomment bellow if you want to save data
+
+#file_name = 'trial_df.xlsx'
+#Trial_df.to_excel(file_name)
+#print('DataFrame is written to Excel File successfully.')
+
+# %%
 SecondTrial = Trial_df.T
 SecondTrial.plot.line(figsize=(15,15))
