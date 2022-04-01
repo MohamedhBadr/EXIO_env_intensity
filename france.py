c469dcb8c1b8dcb4a0196e1795957d9b5682d4c7
Mohamed Badr <62759252+MohamedhBadr@users.noreply.github.com>
2022-04-01 13:45:08 +0200

    Update france.py
    - Included charectirzation factors and now we analyse everything in ghg rather than Co2
    - added some more visualizations 
    - Some issues regarding adding charectirsation factors to the years 2008 and 2020 (maybe related to recession?).

--- france.py
+++ france.py
@@ -9,14 +9,15 @@ and how they have changed over time.
 France was chosen given that it has a significant agricultural sector compared to the rest
 of Europe
 
-Note: The loading and parsining of the EXIOBASE input output tables can take some time. 
+Note: The loading and parsining of the EXIOBASE input output tables can take some time.
+so we use every other year 
 """
 
 # %%
 # Load Libraries
 import pymrio
 import pandas 
-import numpy
+import numpy as np
 import pandas as pd
 
 
@@ -29,48 +30,42 @@ Stressor = 'CO2 - combustion - air'
 # %%
 #load data
 
-#Load EXIOBASE 1995
-exio3_1995 = pymrio.parse_exiobase3(path='exiobase3.4_iot_1995_pxp.zip')
-exio3_1995.calc_all()
 #Load EXIOBASE 1996
 exio3_1996 = pymrio.parse_exiobase3(path='IOT_1996_pxp.zip')
 exio3_1996.calc_all()
-#Load EXIOBASE 1997
-exio3_1997 = pymrio.parse_exiobase3(path='IOT_1997_pxp.zip')
-exio3_1997.calc_all()
 #Load EXIOBASE 1998
 exio3_1998 = pymrio.parse_exiobase3(path='IOT_1998_pxp.zip')
 exio3_1998.calc_all()
-#load EXIOBASE 1999
-exio3_1999 = pymrio.parse_exiobase3(path='IOT_1999_pxp.zip')
-exio3_1999.calc_all()
 #Load EXIOBASE 2000
 exio3_2000 = pymrio.parse_exiobase3(path='IOT_2000_pxp.zip')
 exio3_2000.calc_all()
-#Load EXIOBASE 2001
-exio3_2001 = pymrio.parse_exiobase3(path='IOT_2001_pxp.zip')
-exio3_2001.calc_all()
 #Load EXIOBASE 2002
 exio3_2002 = pymrio.parse_exiobase3(path='IOT_2002_pxp.zip')
 exio3_2002.calc_all()
-#Load EXIOBASE 2003
-exio3_2003 = pymrio.parse_exiobase3(path='IOT_2003_pxp.zip')
-exio3_2003.calc_all()
 #Load EXIOBASE 2004
 exio3_2004 = pymrio.parse_exiobase3(path='IOT_2004_pxp.zip')
 exio3_2004.calc_all()
-#Load EXIOBASE 2005
-exio3_2005 = pymrio.parse_exiobase3(path='IOT_2005_pxp.zip')
-exio3_2005.calc_all()
+#Load EXIOBASE 2006
+exio3_2006 = pymrio.parse_exiobase3(path='IOT_2006_pxp.zip')
+exio3_2006.calc_all()
 #Load EXIOBASE 2008
 exio3_2008 = pymrio.parse_exiobase3(path='IOT_2008_pxp.zip')
 exio3_2008.calc_all()
-#Load EXIOBASE 2011
-exio3_2011 = pymrio.parse_exiobase3(path='exiobase3.4_iot_2011_pxp.zip')
-exio3_2011.calc_all()
+#Load EXIOBASE 2010
+exio3_2010 = pymrio.parse_exiobase3(path='IOT_2010_pxp.zip')
+exio3_2010.calc_all()
+#Load EXIOBASE 2012
+exio3_2012 = pymrio.parse_exiobase3(path='IOT_2012_pxp.zip')
+exio3_2012.calc_all()
+#Load EXIOBASE 2014
+exio3_2014 = pymrio.parse_exiobase3(path='IOT_2014_pxp.zip')
+exio3_2014.calc_all()
 #Load EXIOBASE 2016
 exio3_2016 = pymrio.parse_exiobase3(path='IOT_2016_pxp.zip')
 exio3_2016.calc_all()
+#Load EXIOBASE 2018
+exio3_2018 = pymrio.parse_exiobase3(path='IOT_2018_pxp.zip')
+exio3_2018.calc_all()
 #load EXIOBASE 2020
 exio3_2020 = pymrio.parse_exiobase3(path='IOT_2020_pxp.zip')
 exio3_2020.calc_all()
@@ -78,26 +73,21 @@ exio3_2020.calc_all()
 exio3_2022 = pymrio.parse_exiobase3(path='IOT_2022_pxp.zip')
 exio3_2022.calc_all()
 
-# %%
-
-# %%
 
 # %%
 #Sattelite Accounts
-FR_Multipliers_1995 = exio3_1995.satellite.M[Country]
 FR_Multipliers_1996 = exio3_1996.satellite.M[Country]
-FR_Multipliers_1997 = exio3_1997.satellite.M[Country]
 FR_Multipliers_1998 = exio3_1998.satellite.M[Country]
-FR_Multipliers_1999 = exio3_1999.satellite.M[Country]
 FR_Multipliers_2000 = exio3_2000.satellite.M[Country]
-FR_Multipliers_2001 = exio3_2001.satellite.M[Country]
 FR_Multipliers_2002 = exio3_2002.satellite.M[Country]
-FR_Multipliers_2003 = exio3_2003.satellite.M[Country]
 FR_Multipliers_2004 = exio3_2004.satellite.M[Country]
-FR_Multipliers_2005 = exio3_2005.satellite.M[Country]
+FR_Multipliers_2006 = exio3_2006.satellite.M[Country]
 FR_Multipliers_2008 = exio3_2008.satellite.M[Country]
-FR_Multipliers_2011 = exio3_2011.satellite.M[Country]
+FR_Multipliers_2010 = exio3_2010.satellite.M[Country]
+FR_Multipliers_2012 = exio3_2012.satellite.M[Country]
+FR_Multipliers_2014 = exio3_2014.satellite.M[Country]
 FR_Multipliers_2016 = exio3_2016.satellite.M[Country]
+FR_Multipliers_2018 = exio3_2018.satellite.M[Country]
 FR_Multipliers_2020 = exio3_2020.satellite.M[Country]
 FR_Multipliers_2022 = exio3_2022.satellite.M[Country]
 
@@ -109,50 +99,46 @@ FR_Multipliers_2022 = exio3_2022.satellite.M[Country]
 # It is my understanding that Agricultural products are located 
 #in the first 17 columns (should double check)
 
-FR_Agr_M_1995 = FR_Multipliers_1995.iloc[:, 0:17]
-FR_Agr_M_1996 = FR_Multipliers_1996.iloc[:, 0:17]
-FR_Agr_M_1997 = FR_Multipliers_1997.iloc[:, 0:17]
-FR_Agr_M_1998 = FR_Multipliers_1998.iloc[:, 0:17]
-FR_Agr_M_1999 = FR_Multipliers_1999.iloc[:, 0:17]
-FR_Agr_M_2000 = FR_Multipliers_2000.iloc[:, 0:17]
-FR_Agr_M_2001 = FR_Multipliers_2001.iloc[:, 0:17]
-FR_Agr_M_2002 = FR_Multipliers_2002.iloc[:, 0:17]
-FR_Agr_M_2003 = FR_Multipliers_2003.iloc[:, 0:17]
-FR_Agr_M_2004 = FR_Multipliers_2004.iloc[:, 0:17]
-FR_Agr_M_2005 = FR_Multipliers_2005.iloc[:, 0:17]
-FR_Agr_M_2008 = FR_Multipliers_2008.iloc[:, 0:17]
-FR_Agr_M_2011 = FR_Multipliers_2011.iloc[:, 0:17]
-FR_Agr_M_2016 = FR_Multipliers_2016.iloc[:, 0:17]
-FR_Agr_M_2020 = FR_Multipliers_2020.iloc[:, 0:17]
-FR_Agr_M_2022 = FR_Multipliers_2022.iloc[:, 0:17]
+FR_Agr_M_1996 = FR_Multipliers_1996.iloc[:, 0:15]
+FR_Agr_M_1998 = FR_Multipliers_1998.iloc[:, 0:15]
+FR_Agr_M_2000 = FR_Multipliers_2000.iloc[:, 0:15]
+FR_Agr_M_2002 = FR_Multipliers_2002.iloc[:, 0:15]
+FR_Agr_M_2004 = FR_Multipliers_2004.iloc[:, 0:15]
+FR_Agr_M_2006 = FR_Multipliers_2006.iloc[:, 0:15]
+FR_Agr_M_2008 = FR_Multipliers_2008.iloc[:, 0:15]
+FR_Agr_M_2010 = FR_Multipliers_2010.iloc[:, 0:15]
+FR_Agr_M_2012 = FR_Multipliers_2012.iloc[:, 0:15]
+FR_Agr_M_2014 = FR_Multipliers_2014.iloc[:, 0:15]
+FR_Agr_M_2016 = FR_Multipliers_2016.iloc[:, 0:15]
+FR_Agr_M_2018 = FR_Multipliers_2018.iloc[:, 0:15]
+FR_Agr_M_2020 = FR_Multipliers_2020.iloc[:, 0:15]
+FR_Agr_M_2022 = FR_Multipliers_2022.iloc[:, 0:15]
 
 
 # %%
 
 #Comparing carbon intensities of of the French agricultural sector from 1995-2011
 
-df1 = FR_Agr_M_1995.loc[Stressor]
-df2 = FR_Agr_M_1996.loc[Stressor]
-df3 = FR_Agr_M_1997.loc[Stressor]
-df4 = FR_Agr_M_1998.loc[Stressor]
-df5 = FR_Agr_M_1999.loc[Stressor]
-df6 = FR_Agr_M_2000.loc[Stressor]
-df7 = FR_Agr_M_2001.loc[Stressor]
-df8 = FR_Agr_M_2002.loc[Stressor]
-df9 = FR_Agr_M_2003.loc[Stressor]
-df10 = FR_Agr_M_2004.loc[Stressor]
-df11 = FR_Agr_M_2005.loc[Stressor]
-df12 = FR_Agr_M_2008.loc[Stressor]
-df13 = FR_Agr_M_2011.loc[Stressor]
-df14 = FR_Agr_M_2016.loc[Stressor]
-df15 = FR_Agr_M_2020.loc[Stressor]
-df16 = FR_Agr_M_2022.loc[Stressor]
+df1 = FR_Agr_M_1996.loc[Stressor]
+df2 = FR_Agr_M_1998.loc[Stressor]
+df3 = FR_Agr_M_2000.loc[Stressor]
+df4 = FR_Agr_M_2002.loc[Stressor]
+df5 = FR_Agr_M_2004.loc[Stressor]
+df6 = FR_Agr_M_2006.loc[Stressor]
+df7 = FR_Agr_M_2008.loc[Stressor]
+df8 = FR_Agr_M_2010.loc[Stressor]
+df9 = FR_Agr_M_2012.loc[Stressor]
+df10 = FR_Agr_M_2014.loc[Stressor]
+df11 = FR_Agr_M_2016.loc[Stressor]
+df12 = FR_Agr_M_2018.loc[Stressor]
+df13 = FR_Agr_M_2020.loc[Stressor]
+df14 = FR_Agr_M_2022.loc[Stressor]
 
-dfs = [df1, df2, df3, df4, df5, df6, df7, df8, df9, df10, df11, df12, df13, df14, df15, df16]
+dfs = [df1, df2, df3, df4, df5, df6, df7, df8, df9, df10, df11, df12, df13, df14]
 
 Merged_df = pd.concat(dfs, join='outer', axis=1).fillna(0)
 Merged_df = Merged_df.reset_index()
-Merged_df.columns.values[[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]] = ['1995','1996', '1997','1998', '1999','2000', '2001', '2002','2003','2004', '2005','2008','2011','2016','2020','2022']
+Merged_df.columns.values[[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]] = ['1996','1998', '2000','2002', '2004','2006', '2008', '2010','2012','2014', '2016','2018','2020','2022']
 Merged_df = Merged_df.set_index('sector')
 
 
@@ -168,6 +154,9 @@ Merged_df.plot(kind = 'bar')
 
 Trial_df = Merged_df.drop(index = 'Wool, silk-worm cocoons')
 Trial_df.plot(figsize=(15,15), kind='bar')
+
+SecondTrial = Trial_df.T
+SecondTrial.plot.line(figsize=(15,15))
 
 # %%
 """
@@ -185,26 +174,169 @@ which hence decrease emissions per euro/dollar.
 
 #Follow up question: Despite a general trend of decarbonisation, is this offset by and increase in final demand? 
 """
-# %%
-#Define French Final Demand
-Fr_Y = exio3_1995.Y.loc[Country]
-Fr_agr_Y = Fr_Y[0:17] #agricultural French final demand
 
-
-# %%
-#Lets get Carbon footprint for 1995 from french Agriculture
-#Using D= M@Y (Needs another Look)
-D = FR_Agr_M_1995@Fr_agr_Y
-D
-
+"""
+From the D_cba matrix we can look at the GHG emissions per country
+and plot accordingly 
+ 
+"""
 # %%
 
-#uncomment bellow if you want to save data
+#GET Total GHG emissions
 
-#file_name = 'trial_df.xlsx'
-#Trial_df.to_excel(file_name)
-#print('DataFrame is written to Excel File successfully.')
+charact_table = pd.read_csv('public_char_factors (1).csv',  sep='\t')
+ghg = 'GHG emissions (GWP100) | Problem oriented approach: baseline (CML, 2001) | GWP100 (IPCC, 2007)'
+
+#Get GHG impacts in From French agriculture 1996
+impacts_1996 = exio3_1996.satellite.characterize(charact_table, name="impacts")
+ghg_1996 = impacts_1996.F.loc[[ghg]]
+ghg_1996 = ghg_1996[Country]
+ghg_1996 = ghg_1996.iloc[:, 0:15]
+ghg_1996
+
+#Get GHG impacts in From French agriculture 1998
+impacts_1998 = exio3_1998.satellite.characterize(charact_table, name="impacts")
+ghg_1998 = impacts_1998.F.loc[[ghg]]
+ghg_1998 = ghg_1998[Country]
+ghg_1998 = ghg_1998.iloc[:, 0:15]
+ghg_1998
+
+#Get GHG impacts in From French agriculture 2000
+impacts_2000 = exio3_2000.satellite.characterize(charact_table, name="impacts")
+ghg_2000 = impacts_2000.F.loc[[ghg]]
+ghg_2000 = ghg_2000[Country]
+ghg_2000 = ghg_2000.iloc[:, 0:15]
+ghg_2000
+
+#Get GHG impacts in From French agriculture 2002
+impacts_2002 = exio3_2002.satellite.characterize(charact_table, name="impacts")
+ghg_2002 = impacts_2002.F.loc[[ghg]]
+ghg_2002 = ghg_2002[Country]
+ghg_2002 = ghg_2002.iloc[:, 0:15]
+ghg_2002
+
+#Get GHG impacts in From French agriculture 2004
+impacts_2004 = exio3_2004.satellite.characterize(charact_table, name="impacts")
+ghg_2004 = impacts_2004.F.loc[[ghg]]
+ghg_2004 = ghg_2004[Country]
+ghg_2004 = ghg_2004.iloc[:, 0:15]
+ghg_2004
+
+#Get GHG impacts in From French agriculture 2006
+impacts_2006 = exio3_2006.satellite.characterize(charact_table, name="impacts")
+ghg_2006 = impacts_2006.F.loc[[ghg]]
+ghg_2006 = ghg_2006[Country]
+ghg_2006 = ghg_2006.iloc[:, 0:15]
+ghg_2006
+
+#Get GHG impacts in From French agriculture 2008
+#impacts_2008 = exio3_2008.satellite.characterize(charact_table, name="impacts")
+#ghg_2008 = impacts_2008.F.loc[[ghg]]
+#ghg_2008 = ghg_2008[Country]
+#ghg_2008 = ghg_2008.iloc[:, 0:15]
+#ghg_2008
+
+#Get GHG impacts in From French agriculture 2010
+impacts_2010 = exio3_2010.satellite.characterize(charact_table, name="impacts")
+ghg_2010 = impacts_2010.F.loc[[ghg]]
+ghg_2010 = ghg_2010[Country]
+ghg_2010 = ghg_2010.iloc[:, 0:15]
+ghg_2010
+
+#Get GHG impacts in From French agriculture 2012
+impacts_2012 = exio3_2012.satellite.characterize(charact_table, name="impacts")
+ghg_2012 = impacts_2012.F.loc[[ghg]]
+ghg_2012 = ghg_2012[Country]
+ghg_2012 = ghg_2012.iloc[:, 0:15]
+ghg_2012
+
+#Get GHG impacts in From French agriculture 2014
+impacts_2014 = exio3_2014.satellite.characterize(charact_table, name="impacts")
+ghg_2014 = impacts_2014.F.loc[[ghg]]
+ghg_2014 = ghg_2014[Country]
+ghg_2014 = ghg_2014.iloc[:, 0:15]
+ghg_2014
+
+#Get GHG impacts in From French agriculture 2016
+impacts_2016 = exio3_2016.satellite.characterize(charact_table, name="impacts")
+ghg_2016 = impacts_2016.F.loc[[ghg]]
+ghg_2016 = ghg_2016[Country]
+ghg_2016 = ghg_2016.iloc[:, 0:15]
+ghg_2016
+
+#Get GHG impacts in From French agriculture 2018
+impacts_2018 = exio3_2018.satellite.characterize(charact_table, name="impacts")
+ghg_2018 = impacts_2018.F.loc[[ghg]]
+ghg_2018 = ghg_2018[Country]
+ghg_2018 = ghg_2018.iloc[:, 0:15]
+ghg_2018
+
+#Get GHG impacts in From French agriculture 2020
+#impacts_2020 = exio3_2020.satellite.characterize(charact_table, name="impacts")
+#ghg_2020 = impacts_2020.F.loc[[ghg]]
+#ghg_2020 = ghg_2020[Country]
+#ghg_2020 = ghg_2020.iloc[:, 0:15]
+#ghg_2020
+
+#Get GHG impacts in From French agriculture 2022
+impacts_2022 = exio3_2022.satellite.characterize(charact_table, name="impacts")
+ghg_2022 = impacts_2022.F.loc[[ghg]]
+ghg_2022 = ghg_2022[Country]
+ghg_2022 = ghg_2022.iloc[:, 0:15]
 
 # %%
-SecondTrial = Trial_df.T
-SecondTrial.plot.line(figsize=(15,15))
+#Visualize
+
+data = [ghg_1996,ghg_1998,ghg_2000,ghg_2002,ghg_2004,ghg_2006,ghg_2010,ghg_2012,ghg_2014,ghg_2016,ghg_2018,ghg_2022]
+
+ghg_df = pd.concat(dfs, join='outer', axis=1).fillna(0)
+ghg_df = ghg_df.reset_index()
+ghg_df.columns.values[[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]] = ['1996','1998', '2000','2002', '2004','2006','2010','2012','2014','2016','2018','2022']
+ghg_df = ghg_df.set_index('sector')
+
+#With Wool
+ghg_df.plot(figsize=(15,15), kind='bar')
+
+#without Wool
+Trial_ghg_df = ghg_df.drop(index = 'Wool, silk-worm cocoons')
+Trial_ghg_df.plot(figsize=(15,15), kind='bar')
+
+#Line plot not including wool
+ghg_t = Trial_ghg_df.T
+ghg_t.plot.line(figsize=(15,15))
+
+### NOTE:
+## For some reason the characterization factors function
+## Doesn't work on the years 2008 and 2020 
+## does this have to do with the recession
+
+# %%
+
+#GET Total impacts from consumption based accounting
+
+#1996
+FR_D_cba_1996 = exio3_1996.impacts.D_cba[Country]
+FR_D_cba_agr_1996 = FR_D_cba_1996.iloc[:, 0:15]
+FR_D_cba_agr_ghg_1996 = FR_D_cba_agr_1996.loc[ghg]
+FR_D_cba_agr_ghg_1996
+
+#2022
+FR_D_cba_2022 = exio3_2022.impacts.D_cba[Country]
+FR_D_cba_agr_2022 = FR_D_cba_2022.iloc[:, 0:15]
+FR_D_cba_agr_ghg_2022 = FR_D_cba_agr_2022.loc[ghg]
+FR_D_cba_agr_ghg_2022
+
+#Bar Plot
+D_cba_data = [FR_D_cba_agr_ghg_1996,FR_D_cba_agr_ghg_2022]
+
+D_cba_data_t = pd.concat(D_cba_data, join='outer', axis=1).fillna(0)
+D_cba_data_t = D_cba_data_t.reset_index()
+D_cba_data_t.columns.values[[1, 2]] = ['1996','2022']
+D_cba_data_t = D_cba_data_t.set_index('sector')
+D_cba_data_t.plot(figsize=(10,10),kind = 'bar')
+
+
+
+# %%
+
+# %%
